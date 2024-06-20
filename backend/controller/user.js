b4728c42c4514a133177e3dde0e5ecc14d7afc1c
sujeethcodes <sujeethcodes@gmail.com>
2024-06-20 16:49:02 +0530

    createUserComplete

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,7 +1,19 @@
+const User = require("../model/user")
+const bcrypt = require("bcrypt")
 const controller = {}
-controller.createUser = (req, res) => {
-    res.json("working")
+controller.createUser = async(req, res) => {
+
+    if(!req?.body) return res.json({status:406, message:"values required"})
+
+    const data =  req?.body
+    data.password = bcrypt.hashSync(req?.body?.password, 10)
+  
+    const createUser = await User.create(data)
+    
+    if(!createUser) return res.json({status:406, message:"user create failed"})
+        return res.json({status:200, message:"user created success"})
 }
+
 controller.editUser = (req, res) => {
     res.json("working")
 }
