cf692303a8f4f28657cc2cad60f1b1ecf91e1342
sujeethcodes <sujeethcodes@gmail.com>
2024-06-20 15:29:54 +0530

    setupUser

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,4 +2,13 @@ const controller = {}
 controller.createUser = (req, res) => {
     res.json("working")
 }
+controller.editUser = (req, res) => {
+    res.json("working")
+}
+controller.login = (req, res) => {
+    res.json("working")
+}
+controller.logOut = (req, res) => {
+    res.json("working")
+}
 module.exports =  controller
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const routes = express.Router()
 const userController = require("../controller/user")
+
 routes.post("/createUser", userController.createUser)
+routes.put("/editUser", userController.editUser)
+routes.post("/login", userController.login)
+routes.post("/logout", userController.logOut)
+
 module.exports = routes
